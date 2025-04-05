aa7f036b680dc2ba2e4b398ade5ceff76a73296c
jstanle0 <jstanle0@byu.edu>
2025-04-05 06:56:29 -0600

    rendering react

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return 'Hello World'
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>)
